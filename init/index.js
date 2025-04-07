43ea09c969d10dd5f1eaf8180c9a1f666a3228f3
Abhishek Veer <Abhishekveer21@gmail.com>
2025-04-07 19:03:45 +0530

    Added default image URL for listing schema

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb+srv://wanderlustUser:wanderlustUser%40885681@cluster0.6ajoufe.mongodb.net/wanderlust?retryWrites=true&w=majority&appName=Cluster0";
 
 main()
 .then(() =>{
